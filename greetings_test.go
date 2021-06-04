cb66e5a6be746395b7e7524a7dba9355441a6a4a
d.berezhnoy <moranis.art.media@yandex.ru>
2021-06-04 10:42:42 +0300

    some changes

--- greetings_test.go
+++ greetings_test.go
@@ -15,7 +15,7 @@ func TestHelloName(t *testing.T) {
 	}
 }
 
-func TEstHelloEmpty(t *testing.T) {
+func TestHelloEmpty(t *testing.T) {
 	msg, err := Hello("")
 	if msg != "" || err == nil {
 		t.Fatalf(`Hello("") = %q, %v, want "", error`, msg, err)
